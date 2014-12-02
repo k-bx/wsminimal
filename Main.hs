185638e2900bb52aab1e9486bce0c2b99b991e7e
Konstantine Rybnikov <k-bx@k-bx.com>
2014-12-02 15:12:34 +0200

    Stay listening in WS-socket thread

--- Main.hs
+++ Main.hs
@@ -2,6 +2,10 @@
 
 module Main where
 
+import           Control.Concurrent.MVar
+import           Control.Exception              (fromException, handle)
+import           Control.Monad
+import           Data.List                      (delete)
 import           Data.Monoid
 import           Data.Text                      (Text)
 import qualified Data.Text                      as T
@@ -12,26 +16,76 @@ import qualified Network.Wai.Handler.WebSockets as WaiWS
 import           Network.Wai.Middleware.Static
 import qualified Network.WebSockets             as WS
 import           Prelude                        hiding (log)
+-- import Control.Monad.IO.Class (liftIO)
+
+type AppState = MVar [WS.Connection]
 
 main :: IO ()
-main = run 3001 app
+main = do
+  state <- newMVar []
+  run 3001 (app state)
 
-app :: Application
-app = static $ WaiWS.websocketsOr WS.defaultConnectionOptions wsApp app'
+app :: AppState -> Application
+app state = static $ WaiWS.websocketsOr WS.defaultConnectionOptions (wsApp state) (app' state)
 
-wsApp :: WS.ServerApp
-wsApp pending = do
+wsApp :: AppState -> WS.ServerApp
+wsApp state pending = do
     log $ "Will do WS.acceptRequest"
     conn <- WS.acceptRequest pending
     log $ "Websocket client connected."
+
+    log $ "Putting conn to list of connections"
+    modifyMVar_ state (\l -> return (conn:l))
+    l <- readMVar state
+    log $ "length l: " ++ show (length l)
+
     log $ "Doing receiveData."
     msg <- WS.receiveData conn :: IO Text
     log $ "msg: " <> T.unpack msg
-    return ()
 
-app' :: Application
-app' req respond = respond $
-    responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
+    -- notify everybody known
+    let msg' = "BROADCASTED MESSAGE FROM wsApp. Length is: "
+                 <> T.pack (show (length l)) :: Text
+    broadcast state msg'
+
+    talk conn state
+  where
+    talk :: WS.Connection -> AppState -> IO ()
+    talk conn state = handle catchDisconnect $
+        forever $ do
+            msg <- WS.receiveData conn
+            l <- readMVar state
+            broadcast state ("Someone sent a message: " <> msg)
+      where
+        catchDisconnect e = case fromException e of
+            Just WS.ConnectionClosed -> do
+                log $ "Should disconnect someone (ideally)"
+                broadcast state "Someone disconnected"
+                removeClient conn state
+            _ -> return ()
+
+removeClient :: WS.Connection -> AppState -> IO ()
+removeClient conn state = return ()
+  -- modifyMVar_ state $ \l -> return (delete conn l)
+
+broadcast :: AppState -> Text -> IO ()
+broadcast s msg = do
+    l <- readMVar s
+    forM_ l $ \conn -> do
+        log $ "Sending broadcast"
+        WS.sendTextData conn msg
+
+app' :: AppState -> Application
+app' state req respond = do
+    -- broadcast on post
+    when (requestMethod req == "POST") $ do
+        log $ "Got POST request. Will send message to all clients"
+        l <- readMVar state
+        log $ "length l: " ++ show (length l)
+        let msg = "BROADCASTED MESSAGE FROM wsApp. Length is: "
+                    <> T.pack (show (length l)) :: Text
+        broadcast state msg
+    respond $ responseLBS status200 [("Content-Type", "text/plain")] "Hello World"
 
 log :: String -> IO ()
 log = putStrLn
